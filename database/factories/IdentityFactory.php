e913de2e35fa72ed96775289a5ae7a6f4a3426f9
Alessio <alessio.vertemati@gmail.com>
2020-08-11 13:21:01 +0200

    Define Identity factory

--- database/factories/IdentityFactory.php
+++ database/factories/IdentityFactory.php
@@ -2,11 +2,16 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\User;
 use App\Identity;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
 $factory->define(Identity::class, function (Faker $faker) {
     return [
-        //
+        'provider' => $faker->name,
+        'provider_id' => $faker->name,
+        'user_id' => factory(User::class),
+        'token' => Str::random(10),
     ];
 });
