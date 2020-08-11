f393988d38c0f88e49fa017e644cd013a788d6f2
Alessio <alessio.vertemati@gmail.com>
2020-08-11 13:20:31 +0200

    Use generic Gitlab name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
 
         <div class="mt-6">
             <a href="{{ route('connect.provider', ['provider' => 'gitlab']) }}"  class="relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
-                {{ __('Login with Gitlab.com') }}
+                {{ __('Login with Gitlab') }}
             </a>
         </div>
 
