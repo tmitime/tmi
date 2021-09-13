1ea759ca169b991c797ee3352c47267d23cc2ac2
Alessio <alessio.vertemati@gmail.com>
2021-09-13 21:19:11 +0200

    Redirects welcome to login

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'login');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', DashboardController::class)->name('dashboard');
 
