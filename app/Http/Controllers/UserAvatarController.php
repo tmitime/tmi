39389ad0172e979d1e9810ad995c85bb1955bed8
Alessio <avvertix@users.noreply.github.com>
2021-11-09 21:50:39 +0100

    Make default avatars unique (#20)
    * Use user based avatar url
    * Use local Montserrat font file

--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\HeaderUtils;
@@ -16,23 +17,25 @@ class UserAvatarController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, $avatar = null)
     {
-        $user = $request->user();
+        $user_id = @hex2bin($avatar) ?: null;
 
-        $initials = Str::of($user->name)->limit(2, '')->title();
+        $user = $user_id ? User::find($user_id) : null;
+
+        $initials = Str::of(optional($user)->name ?? ':(')->limit(2, '')->title();
 
         $background = '#65A30D';
         $foreground = '#fff';
+        $font = url('fonts/montserrat-latin.woff2');
 
-        // TODO: Swap Google Fonts with a local version
         // TODO: Remeber that the used unicode subset is equal to latin
 
         $svg = <<<"SVG"
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="font-weight:700;" width="500px" height="500px">
             <defs>
                 <style type="text/css">
-                    @font-face {font-family: "montserratbold";src: url("https://fonts.gstatic.com/s/montserrat/v18/JTURjIg1_i6t8kCHKm45_dJE3gnD_g.woff2") format("woff2");}
+                    @font-face {font-family: "Montserrat";src: url("{$font}") format("woff2");}
                 </style>
             </defs>
             <rect x="0" y="0" width="500" height="500" rx="0" style="fill:{$background}"/>
