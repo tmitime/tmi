b262a53ba1bd322d23c091ae07c388a69d3eb3a7
Alessio <alessio.vertemati@gmail.com>
2021-11-09 21:41:01 +0100

    Use local Montserrat font file

--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -27,15 +27,15 @@ class UserAvatarController extends Controller
 
         $background = '#65A30D';
         $foreground = '#fff';
+        $font = url('fonts/montserrat-latin.woff2');
 
-        // TODO: Swap Google Fonts with a local version
         // TODO: Remeber that the used unicode subset is equal to latin
 
         $svg = <<<"SVG"
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="font-weight:700;" width="500px" height="500px">
             <defs>
                 <style type="text/css">
-                    @font-face {font-family: "montserratbold";src: url("https://fonts.gstatic.com/s/montserrat/v18/JTURjIg1_i6t8kCHKm45_dJE3gnD_g.woff2") format("woff2");}
+                    @font-face {font-family: "Montserrat";src: url("{$font}") format("woff2");}
                 </style>
             </defs>
             <rect x="0" y="0" width="500" height="500" rx="0" style="fill:{$background}"/>
