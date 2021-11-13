09827ea57b9baae8b50d742418393d4ce1729bb1
Alessio <alessio.vertemati@gmail.com>
2021-11-13 15:20:14 +0100

    Use same stylying for project details section

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -8,36 +8,31 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
-<div class="md:grid md:grid-cols-3 md:gap-6">
-    <x-jet-section-title>
-        <x-slot name="title">{{ __('Project details') }}</x-slot>
-        <x-slot name="description">
-            {{ __('Specify the name and the starting date to create a project.') }}<br/>
-            {{ __('You can also indicate the expected working days if planned.') }}
-        </x-slot>
-    </x-jet-section-title>
+            <div class="md:grid md:grid-cols-3 md:gap-6">
+                <x-jet-section-title>
+                    <x-slot name="title">{{ __('Project details') }}</x-slot>
+                    <x-slot name="description">
+                        {{ __('Specify the name and the starting date to create a project.') }}<br/>
+                        {{ __('You can also indicate the expected working days if planned.') }}
+                    </x-slot>
+                </x-jet-section-title>
 
-    <div class="mt-5 md:mt-0 md:col-span-2">
-        <form action="{{ route('projects.store') }}" method="post">
-            
-            @csrf
+                <div class="mt-5 md:mt-0 md:col-span-2">
+                    <form action="{{ route('projects.store') }}" method="post">
+                        <div class="px-4 py-5 bg-white sm:p-6 shadow sm:rounded-tl-md sm:rounded-tr-md">
+                            @csrf
 
-            @include('projects.partials.details-form')
+                            @include('projects.partials.details-form')
+                        </div>
 
-            <div class="flex items-center justify-end mt-4">
-
-                <x-jet-button class="ml-4">
-                    {{ __('Create project') }}
-                </x-jet-button>
+                        <div class="flex items-center justify-end px-4 py-3 bg-gray-50 text-right sm:px-6 shadow sm:rounded-bl-md sm:rounded-br-md">
+                            <x-jet-button class="ml-4">
+                                {{ __('Create project') }}
+                            </x-jet-button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-        </form>
-    </div>
-</div>
-
-
-                    
-
         </div>
     </div>
 </x-app-layout>
