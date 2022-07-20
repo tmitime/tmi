93a01c02829be4b65d5dd1a55e21090e77048a37
Alessio <avvertix@users.noreply.github.com>
2022-07-20 21:09:58 +0200

    Switch to Vite and upgrade Tailwind CSS (#31)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,16 +7,9 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-
         @livewireStyles
-
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans">
         <x-jet-banner />
