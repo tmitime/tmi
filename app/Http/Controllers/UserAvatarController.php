d0d04dfcf5f0e2cd6cd30edab563444fc6e24413
Alessio <avvertix@users.noreply.github.com>
2025-08-03 21:25:56 +0200

    Laravel 12 upgrade (#68)
    * L12 upgrade - dependencies
    
    * Update code style fixer version
    
    * Use MariaDB 10.11 and drop PHP 8.2
    
    * Use PHP 8.3 during build
    
    * Fix workdir
    
    * Update JS dependencies
    
    * Update JS dependencies
    
    * Use connect-identity release
    
    * Sync files with Laravel base
    
    * Update defaults
    
    * Fix styling
    
    * Simplify test using in-memory sqlite database
    
    ---------
    
    Co-authored-by: avvertix <5672748+avvertix@users.noreply.github.com>

--- app/Http/Controllers/UserAvatarController.php
+++ app/Http/Controllers/UserAvatarController.php
@@ -22,13 +22,13 @@ class UserAvatarController extends Controller
 
         $user = $user_id ? User::find($user_id) : null;
 
-        $initials = Str::of(optional($user)->name ?? ':(')->limit(2, '')->title();
+        $initials = Str::of(optional($user)->name ?? ':(')->limit(2, '')->title()->transliterate()->ascii();
+
+        // Unicode subset is equal to latin
 
         $background = '#65A30D';
         $foreground = '#fff';
         $font = url('fonts/montserrat-latin.woff2');
-
-        // TODO: Remeber that the used unicode subset is equal to latin
 
         $svg = <<<"SVG"
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="font-weight:700;" width="500px" height="500px">
